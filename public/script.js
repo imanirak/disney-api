3f39732387f6e11c415c14f738dcf07034d64ac3
Karina <karinadgarces@gmail.com>
2022-02-26 13:36:34 -0500

    search bar working, case sensitive

--- public/script.js
+++ public/script.js
@@ -3,7 +3,11 @@ const searchBtn = document.getElementById('charSearchBtn');
 let searchValue = document.getElementById("search");
 
  
-  
+     
+searchBtn.addEventListener("click", () =>{
+    const search = searchValue.value;
+//   console.log("btn works" + search)
+
   const promise = $.ajax({
     url: `https://api.disneyapi.dev/characters`,
   });
@@ -11,46 +15,60 @@ let searchValue = document.getElementById("search");
   promise.then(
     (disneyData) => {
       const data = disneyData.data;
-      
-      searchBtn.addEventListener("click", () =>{
-        const search = searchValue.value;
-      console.log(`button works and grabs value ${search}`)
-      
+
+   //filter through api
  for(let i=0; i< data.length;i++){
-    if(search === data[i].name){
-        
-       console.log(`${search} and ${data[i].name} are the same`)
+   
+    //find matching name value
+if(search === data[i].name){
 
-    }
-const disneyImage = data[i].imageUrl;
-   document.getElementById("character").src = disneyImage;
-  console.log(data[i])
-   $('#name').html(data[i].name);
-  $('#details').html(data[i].tvShows);
-//    $('#rated').html(data.Rated);
+    //view character img
+    const disneyImage = data[i].imageUrl;
+    document.getElementById("character").src = disneyImage;
+   console.log(data[i])
+    $('#name').html(data[i].name);
+   $('#details').html(data[i].tvShows);
+    $('#shortFilms').html(data[i].shortFilms);
+
 }
-    }) 
-
-
-//  for(let i=0; i< data.length;i++){
-//      if(search === data[i].name){
-         
-//         console.log(`${search} and ${data[i].name} are the same`)
-
-//      }
-//  const disneyImage = data[i].imageUrl;
-//     document.getElementById("character").src = disneyImage;
-//    console.log(data[i])
-//     $('#name').html(data[i].name);
-//    $('#details').html(data[i].tvShows);
-// //    $('#rated').html(data.Rated);
-//  } 
+ } 
     },
     (error) => {
       console.log("bad request: ", error);
     }
    
   );
+
+}) 
   
 
 
+
+
+
+
+
+//   const promise = $.ajax({
+//     url: `https://api.disneyapi.dev/characters`,
+//   });
+  
+//   promise.then(
+//     (disneyData) => {
+//       const data = disneyData.data;
+   
+
+//  for(let i=0; i< data.length;i++){
+
+//  const disneyImage = data[i].imageUrl;
+//     document.getElementById("character").src = disneyImage;
+//    console.log(data[i])
+//     $('#name').html(data[i].name);
+//    $('#details').html(data[i].tvShows);
+
+//  } 
+//     },
+//     (error) => {
+//       console.log("bad request: ", error);
+//     }
+   
+//   );
