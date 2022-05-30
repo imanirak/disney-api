652eafdf124f3a631288f3d6e5dafb5893428fb7
Karina <karinadgarces@gmail.com>
2022-05-30 16:09:09 -0400

    fix search bar

--- public/script.js
+++ public/script.js
@@ -2,12 +2,10 @@
 const searchBtn = document.getElementById('charSearchBtn');
 let searchValue = document.getElementById("search");
 
- 
-     
+  
 searchBtn.addEventListener("click", () =>{
     const search = searchValue.value;
-//   console.log("btn works" + search)
-
+ 
   const promise = $.ajax({
     url: `https://api.disneyapi.dev/characters/`,
   });
@@ -18,14 +16,16 @@ searchBtn.addEventListener("click", () =>{
 
    //filter through api
  for(let i=0; i< data.length;i++){
-   console.log(data[i])
-    //find matching name value
-if(search === data[i].name){
+  //  console.log(data[i])
 
+console.log(search + data[i].name)
+    //find matching name value
+if(search == data[i].name.toLowerCase()){
+console.log(data[i].name.toLowerCase())
     //view character img
     const disneyImage = data[i].imageUrl;
     document.getElementById("character").src = disneyImage;
-   console.log(data[i])
+   console.log(data[i].name)
     $('#name').html(data[i].name);
    $('#details').html(data[i].tvShows);
     $('#shortFilms').html(data[i].shortFilms);
